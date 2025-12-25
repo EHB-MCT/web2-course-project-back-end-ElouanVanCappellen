71b1d02381ea441ea18ca4bed63c8927c99f6583
ElouanVanCappellen <elouan.van.cappellen@student.ehb.be>
2025-12-25 15:27:23 +0100

    basic test express

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const PORT = 3000;
 
 app.use(cors());              // allow requests from front-end
 app.use(express.json());      // read JSON bodies
+
+app.post('/login', (req, res) => {
+    res.send({ message: "Hello world" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
